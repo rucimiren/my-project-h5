3f72ffef8e17571d0700f3692c821f95bfcb408c
ext.xiaoqiang <ext.xiaoqiang@jd.com>
2021-12-20 12:03:38 +0800

    create

---
+++ src/views/home/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <div id="nav">
+      <router-link to="/drag">drag</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+<style lang="less" scoped>
+.home {
+  font-size: 20px;
+  #nav {
+    padding: 30px;
+
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+  }
+}
+</style>
